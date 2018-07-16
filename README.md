0197970f28a35c8571e0b8c1115400040777f444
Stoeffel <schtoeffel@gmail.com>
2018-07-16 18:16:58 +0200

    :sunrise:

---
+++ README.md
@@ -0,0 +1,15 @@
+# string-conversions
+
+String conversion functions for Elm.
+
+## Testing
+
+```
+npm i
+npm test
+```
+
+## License
+
+The source code for this package is released under the terms of the BSD3
+license. See the `LICENSE` file.
---
+++ src/String/Conversions.elm
@@ -0,0 +1,339 @@
+module String.Conversions
+    exposing
+        ( fromBool
+        , fromDay
+        , fromDict
+        , fromHttpError
+        , fromHttpResponse
+        , fromList
+        , fromMaybe
+        , fromMonth
+        , fromRecord
+        , fromSet
+        , fromString
+        , fromTuple2
+        , fromTuple3
+        , fromValue
+        , withUnionConstructor
+        )
+
+{-| Helpers to convert common types into a `String`.
+
+@docs fromBool
+@docs fromDay
+@docs fromDict
+@docs fromHttpError
+@docs fromHttpResponse
+@docs fromList
+@docs fromMaybe
+@docs fromMonth
+@docs fromRecord
+@docs fromSet
+@docs fromString
+@docs fromTuple2
+@docs fromTuple3
+@docs fromValue
+@docs withUnionConstructor
+
+-}
+
+import Date
+import Dict exposing (Dict)
+import Http
+import Json.Encode as Encode exposing (Value)
+import Set exposing (Set)
+
+
+{-| Nest some arguments under a tag, including parentheses when needed. Helpful for printing union type values.
+
+    import String.Extra exposing (fromInt)
+
+    withUnionConstructor "Ok" [ fromInt 1 ]
+    --> "Ok 1"
+
+-}
+withUnionConstructor : String -> List String -> String
+withUnionConstructor tag args =
+    let
+        needsParens a =
+            String.contains " " a
+                && not (String.startsWith "{" a)
+                && not (String.startsWith "(" a)
+                && not (String.startsWith "[" a)
+                && not (String.startsWith "\"" a)
+                && not (String.startsWith "'" a)
+
+        argsString =
+            args
+                |> List.map
+                    (\arg ->
+                        if needsParens arg then
+                            "(" ++ arg ++ ")"
+                        else
+                            arg
+                    )
+                |> String.join " "
+    in
+    tag ++ " " ++ argsString
+
+
+{-| Build a record string for debugging and logging with pairs of
+keys and string conversion functions, meant to be paired with accessors
+
+    import String.Extra exposing (fromInt)
+
+    fromRecord [ ("hello", .hello >> fromInt ) ] { hello = 1 }
+    --> "{ hello = 1 }"
+
+-}
+fromRecord : List ( String, a -> String ) -> a -> String
+fromRecord fields record =
+    let
+        inner =
+            fields
+                |> List.map
+                    (\( key, fieldToString ) ->
+                        key ++ " = " ++ fieldToString record
+                    )
+                |> String.join ", "
+    in
+    "{ " ++ inner ++ " }"
+
+
+{-| Convert a 2-tuple to a string using nested conversions.
+
+    import String.Extra exposing (fromFloat, fromInt)
+
+    fromTuple2 fromInt fromFloat ( 1, 1.5 )
+    --> "(1,1.5)"
+
+-}
+fromTuple2 : (a -> String) -> (b -> String) -> ( a, b ) -> String
+fromTuple2 leftToString rightToString ( a, b ) =
+    "(" ++ leftToString a ++ "," ++ rightToString b ++ ")"
+
+
+{-| Convert a 3-tuple to a string using nested conversions.
+
+    import String.Extra exposing (fromInt, fromFloat)
+
+    fromTuple3 fromInt fromFloat fromInt ( 1, 1.5, 2 )
+    --> "(1,1.5,2)"
+
+-}
+fromTuple3 : (a -> String) -> (b -> String) -> (c -> String) -> ( a, b, c ) -> String
+fromTuple3 firstToString secondToString thirdToString ( a, b, c ) =
+    "(" ++ firstToString a ++ "," ++ secondToString b ++ "," ++ thirdToString c ++ ")"
+
+
+{-| Convert a List to a string using a nested conversion.
+
+    import String.Extra exposing (fromInt)
+
+    fromList fromInt [1, 2, 3]
+    --> "[1,2,3]"
+
+-}
+fromList : (a -> String) -> List a -> String
+fromList innerToString list =
+    let
+        contents =
+            list
+                |> List.map innerToString
+                |> String.join ","
+    in
+    "[" ++ contents ++ "]"
+
+
+{-| Convert a Dict to a String using nested conversions.
+
+    import Dict
+    import String.Extra exposing (fromFloat, fromInt)
+
+    fromDict fromInt fromFloat (Dict.fromList [(1, 1.5)])
+    --> "Dict.fromList [(1,1.5)]"
+
+-}
+fromDict : (comparable -> String) -> (v -> String) -> Dict comparable v -> String
+fromDict keyToString valueToString dict =
+    let
+        contents =
+            dict
+                |> Dict.toList
+                |> fromList (fromTuple2 keyToString valueToString)
+    in
+    "Dict.fromList " ++ contents
+
+
+{-| Convert a Set to a string using a nested conversion.
+
+    import Set
+    import String.Extra exposing (fromInt)
+
+    fromSet fromInt (Set.fromList [1, 2])
+    --> "Set.fromList [1,2]"
+
+-}
+fromSet : (comparable -> String) -> Set comparable -> String
+fromSet innerToString set =
+    let
+        contents =
+            set
+                |> Set.toList
+                |> fromList innerToString
+    in
+    "Set.fromList " ++ contents
+
+
+{-| Convert a String to a debugging version of that String.
+
+    fromString "hello \"world\""
+    --> "\"hello \\\"world\\\"\""
+
+-}
+fromString : String -> String
+fromString string =
+    Encode.encode 0 (Encode.string string)
+
+
+{-| Convert an Http.Error to a String.
+-}
+fromHttpError : Http.Error -> String
+fromHttpError error =
+    case error of
+        Http.BadUrl url ->
+            withUnionConstructor "BadUrl" [ fromString url ]
+
+        Http.Timeout ->
+            withUnionConstructor "Timeout" []
+
+        Http.NetworkError ->
+            withUnionConstructor "NetworkError" []
+
+        Http.BadStatus response ->
+            withUnionConstructor "BadStatus" [ fromHttpResponse response ]
+
+        Http.BadPayload message response ->
+            withUnionConstructor "BadPayload " [ fromString message, fromHttpResponse response ]
+
+
+{-| Convert an Http.Response String to a String.
+-}
+fromHttpResponse : Http.Response String -> String
+fromHttpResponse =
+    fromRecord
+        [ ( "url", .url >> fromString )
+        , ( "status"
+          , .status
+                >> fromRecord
+                    [ ( "code", .code >> toString ) -- TODO String.fromInt
+                    , ( "message", .message >> fromString )
+                    ]
+          )
+        , ( "headers", .headers >> fromDict fromString fromString )
+        , ( "body", .body >> fromString )
+        ]
+
+
+{-| Convert a Date.Month to a String matching its constructor.
+-}
+fromMonth : Date.Month -> String
+fromMonth month =
+    case month of
+        Date.Jan ->
+            "Jan"
+
+        Date.Feb ->
+            "Feb"
+
+        Date.Mar ->
+            "Mar"
+
+        Date.Apr ->
+            "Apr"
+
+        Date.May ->
+            "May"
+
+        Date.Jun ->
+            "Jun"
+
+        Date.Jul ->
+            "Jul"
+
+        Date.Aug ->
+            "Aug"
+
+        Date.Sep ->
+            "Sep"
+
+        Date.Oct ->
+            "Oct"
+
+        Date.Nov ->
+            "Nov"
+
+        Date.Dec ->
+            "Dec"
+
+
+{-| Convert a Date.Day to a String matching its constructor.
+-}
+fromDay : Date.Day -> String
+fromDay day =
+    case day of
+        Date.Mon ->
+            "Mon"
+
+        Date.Tue ->
+            "Tue"
+
+        Date.Wed ->
+            "Wed"
+
+        Date.Thu ->
+            "Thu"
+
+        Date.Fri ->
+            "Fri"
+
+        Date.Sat ->
+            "Sat"
+
+        Date.Sun ->
+            "Sun"
+
+
+{-| Convert a Maybe to a String using a nested conversion.
+
+    import String.Extra exposing (fromInt)
+
+    fromMaybe fromInt (Just 1)
+    --> "Just 1"
+
+-}
+fromMaybe : (a -> String) -> Maybe a -> String
+fromMaybe innerToString maybe =
+    case maybe of
+        Just a ->
+            withUnionConstructor "Just" [ innerToString a ]
+
+        Nothing ->
+            "Nothing"
+
+
+{-| Convert a Bool to a String matching its constructor.
+-}
+fromBool : Bool -> String
+fromBool bool =
+    if bool then
+        "True"
+    else
+        "False"
+
+
+{-| Convert a Json.Decode.Value to a JSON String.
+-}
+fromValue : Value -> String
+fromValue value =
+    Encode.encode 0 value
